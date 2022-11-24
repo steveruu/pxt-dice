367943401d9dab91876dd85c9946a0436ef3f427
steveruu <39307626+steveruu@users.noreply.github.com>
2022-11-24 10:59:57 +0100

    functional, optimized code

--- main.ts
+++ main.ts
@@ -1,6 +1,20 @@
-basic.forever(function () {
+let i = 0;
 
+input.onButtonPressed(Button.B, function () {
+    i += 1;
+    if (i > 10) {
+        i = 10;
+    }
+    basic.showNumber(i);
 })
 
+input.onButtonPressed(Button.A, function () {
+    i -= 1;
+    if (i < 0) {
+        i = 0;
+    }
+    basic.showNumber(i);
+})
 
-basic.showNumber(12)
+basic.forever(function () {
+})
