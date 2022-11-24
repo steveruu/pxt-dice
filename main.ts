7c42634e8b81329361a4eb394eba9fb03c4add22
steveruu <39307626+steveruu@users.noreply.github.com>
2022-11-24 10:29:07 +0100

    commit test

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-for (let index = 0; index < 4; index++) {
-    music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 1, 1, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
-}
 basic.forever(function () {
-	
+
 })
+
+
+basic.showNumber(12)
