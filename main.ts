6c767281db4137388a4415f63e9862f0c69d52da
steveruu <39307626+steveruu@users.noreply.github.com>
2022-11-24 11:34:27 +0100

    commit dodelat doma; su dement :)

--- main.ts
+++ main.ts
@@ -1,20 +1,25 @@
 let i = 0;
-
-input.onButtonPressed(Button.B, function () {
-    i += 1;
-    if (i > 10) {
-        i = 10;
-    }
-    basic.showNumber(i);
-})
-
-input.onButtonPressed(Button.A, function () {
-    i -= 1;
-    if (i < 0) {
-        i = 0;
-    }
-    basic.showNumber(i);
-})
+let wasPressed_A: boolean = false;
+let wasPressed_B: boolean = false;
 
 basic.forever(function () {
+    if (input.buttonIsPressed(Button.B) && (i < 10)) {
+        i += 1;
+
+    }
+    
+    if (input.buttonIsPressed(Button.A) && (i > 0)) {
+        i -= 1;
+    }
+
+    basic.pause(300);
+    whaleysans.showNumber(i);
 })
+
+// loops.everyInterval(300, function() {
+//     if (input.buttonIsPressed(Button.A)) {
+//         wasPressed_A = true;
+//     }
+//     if (input.buttonIsPressed(Button.B))
+//         wasPressed_B = true;
+// })
